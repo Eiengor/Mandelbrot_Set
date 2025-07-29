72a2d9a5d6e6ebd8fbf59f0cea3691632066ce28
Oleh <memchik2017@gmail.com>
2025-07-29 20:37:36 +0300

    Added Resources.h for clearness

---
+++ Mandelbrot_Set/Resources.h
@@ -0,0 +1,52 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <glm/glm.hpp>
+#include <vector>
+#include <chrono>
+#include <numeric>
+
+#include "VAO.h"
+#include "VBO.h"
+#include "EBO.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "ComputeShader.h"
+#include "Application.h"
+#include "Renderer.h"
+#include "InputHandler.h"
+
+int screen_width = 1600;
+int screen_height = 900;
+
+int num_frames = 0;
+float last_time = 0.0f;
+float center_x = 0.0f;
+float center_y = 0.0f;
+float zoom = 1.0f;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+float vertices[] = {
+    //    x      y      z   
+        -1.0f, -1.0f, -0.0f,
+         1.0f,  1.0f, -0.0f,
+        -1.0f,  1.0f, -0.0f,
+         1.0f, -1.0f, -0.0f
+};
+
+unsigned int indices[] = {
+        0, 1, 2,
+        0, 3, 1
+};
+
+#endif // !RESOURCES_H
+
--- Mandelbrot_Set/main.cpp
+++ Mandelbrot_Set/main.cpp
@@ -1,52 +1,7 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-#include <math.h>
-#include <algorithm>
-#include <glm/glm.hpp>
-#include <vector>
-#include <chrono>
-#include <numeric>
-
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
-#include "Shader.h"
-#include "Texture.h"
-#include "ComputeShader.h"
-#include "Application.h"
-#include "Renderer.h"
-#include "InputHandler.h"
+#include "Resources.h"
 
 using namespace std;
 using namespace glm;
-
-int screen_width = 1600;
-int screen_height = 900;
-
-int num_frames = 0;
-float last_time = 0.0f;
-float center_x = 0.0f;
-float center_y = 0.0f;
-float zoom = 1.0f;
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
-
-float vertices[] = {
-    //    x      y      z   
-        -1.0f, -1.0f, -0.0f,
-         1.0f,  1.0f, -0.0f,
-        -1.0f,  1.0f, -0.0f,
-         1.0f, -1.0f, -0.0f
-};
-
-unsigned int indices[] = {
-        0, 1, 2,
-        0, 3, 1
-};
 
 void countFPS() {
     double current_time = glfwGetTime();
